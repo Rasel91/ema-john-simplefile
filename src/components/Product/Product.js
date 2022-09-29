2c3cd72bb25a4f10db4045ff4252abb0a24b5d69
Rasel91 <srasel.dpe@gmail.com>
2022-09-29 13:02:52 +0600

    add to cart btn

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,10 +6,15 @@ const Product = (props) => {
     return (
         <div className='product'>
            <img src={img} alt="" />
-           <h6>Product Name:{name}</h6>
-           <p>Ratings:{ratings}</p>
-           <p>Seller:{seller}</p>
-           <p>Price:{price}</p>
+          <div className='product-info'>
+          <h6>{name}</h6>
+           <h6>Price:{price}</h6>
+           <p><small>Ratings:{ratings}</small></p>
+           <p><small>Seller:{seller}</small></p>
+          </div>
+           <button className='button-cart'>
+            <p>Add to Cart</p>
+           </button>
         </div>
     );
 };
